ad44873ec1b636b768bcef3e547cabf2e0d7ecfc
liufei <liufei@chupinxiu.com>
2017-09-04 16:45:58 +0800

    index body

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,7 +2,6 @@ const Base = require('./base.js');
 
 module.exports = class extends Base {
   indexAction() {
-  	console.log(this.state);
-    return this.display();
+  	this.body = 'hello thinkjs';
   }
 };
